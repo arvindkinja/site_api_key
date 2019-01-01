6fa2f140b4887835bb5c3c3146747bf8b28758b8
arvindkinja <arvindkinja@gmail.com>
2019-01-02 02:26:18 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,27 @@
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Requirements
+ * Installation
+ * Configuration
+
+INTRODUCTION
+------------
+Provide additional field to store site api Key in site information form.
+
+REQUIREMENTS
+------------
+No special requirements.
+
+INSTALLATION
+------------
+ * Install as you would normally install a contributed Drupal module.
+   See: https://www.drupal.org/docs/user_guide/en/config-install.html for further information.
+
+
+CONFIGURATION
+-------------
+You can add new site key. Please follow below steps to add new api key
+Navigate admin >> configuration >> System >> Basic site settings
+admin/config/system/site-information
---
+++ src/Controller/ContentViewController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\site_api_key\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Defines a controller that will return a single node json response.
+ */
+class ContentViewController implements ContainerInjectionInterface {
+
+  /**
+   * The configuration service
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private $config;
+
+  /**
+   * The Entity Type Manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Creates an ContentViewController object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct( ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager ) {
+    $this->config = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create( ContainerInterface $container ) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+
+  /**
+   * Return json response for a given node ID
+   * @param $api_key The key of the site
+   * @param $nid The ID of the node
+   */
+  public function view( $api_key, $nid ) {
+    // Get the site api key from the db
+    $site_api_key = $this->config->get('siteapikey.settings');
+    $key = $site_api_key->get('siteapikey');
+
+    // Check the api_key is valid or not
+    if ( $api_key != '' && $api_key != $key ) {
+      throw new AccessDeniedHttpException();
+    }
+
+    // Load node object by node ID from db 
+    $node_storage = $this->entityTypeManager->getStorage('node');
+    $node = $node_storage->load( $nid );
+
+    // Check node is exists in the db or not
+    if ($node == null) {
+      throw new AccessDeniedHttpException();
+    }
+
+    // Convert object in array
+    $response = $node->toArray();
+
+    // Return node result in json format
+    return new JsonResponse($response);
+
+  }
+}
