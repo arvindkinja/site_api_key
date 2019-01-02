cb4a63d59ef3d9b3f5b81dd969ac058851654096
arvindkinja <arvindkinja@gmail.com>
2019-01-02 06:00:08 +0530

    Fixed code formating using code sniffer.

--- src/Controller/ContentViewController.php
+++ src/Controller/ContentViewController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 class ContentViewController implements ContainerInjectionInterface {
 
   /**
-   * The configuration service
+   * The configuration service.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
@@ -36,7 +36,7 @@ class ContentViewController implements ContainerInjectionInterface {
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    */
-  public function __construct( ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager ) {
+  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
     $this->config = $config_factory;
     $this->entityTypeManager = $entity_type_manager;
   }
@@ -44,48 +44,51 @@ class ContentViewController implements ContainerInjectionInterface {
   /**
    * {@inheritdoc}
    */
-  public static function create( ContainerInterface $container ) {
+  public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
       $container->get('entity_type.manager')
     );
   }
 
-
   /**
-   * Return json response for a given node ID
-   * @param $api_key The key of the site
-   * @param $nid The ID of the node
+   * Return json response for a given node ID.
+   *
+   * @param $api_key String
+   *   The key of the site
+   * @param $nid
+   *   The ID of the node
    */
-  public function view( $api_key, $nid ) {
-    // Get the site api key from the db
+  public function view($api_key, $nid) {
+    // Get the site api key from the db.
     $site_api_key = $this->config->get('siteapikey.settings');
     $key = $site_api_key->get('siteapikey');
 
-    // Check the api_key is valid or not
-    if ( $api_key != '' && $api_key != $key ) {
+    // Check the api_key is valid or not.
+    if ($api_key != '' && $api_key != $key) {
       throw new AccessDeniedHttpException();
     }
 
-    // Load node object by node ID from db 
+    // Load node object by node ID from db.
     $node_storage = $this->entityTypeManager->getStorage('node');
-    $node = $node_storage->load( $nid );
+    $node = $node_storage->load($nid);
 
-    // Check node is exists in the db or not
-    if ($node == null) {
+    // Check node is exists in the db or not.
+    if ($node == NULL) {
       throw new AccessDeniedHttpException();
     }
 
-    // Check the node is published or not
-    if ( ! $node->isPublished() ) {
+    // Check the node is published or not.
+    if (!$node->isPublished()) {
       throw new AccessDeniedHttpException();
     }
 
-    // Convert object in array
+    // Convert object in array.
     $response = $node->toArray();
 
-    // Return node result in json format
+    // Return node result in json format.
     return new JsonResponse($response);
 
   }
+
 }
